211410585f67368d1963990e291be77920ffe8b9
MissKaren18 <93577516+MissKaren18@users.noreply.github.com>
2023-03-02 00:07:08 -0600

    Add files via upload

---
+++ botones.js
@@ -0,0 +1,15 @@
+function fortnite(){
+    window.open  ("https://www.fortnite.com/?lang=es-MX")
+}
+
+function roblox(){
+    window.location = "https://www.roblox.com/"
+}
+
+function marioKart(){
+    window.location = "https://mariokart8.nintendo.com/es/"
+}
+
+function minecraft(){
+    window.location = "https://education.minecraft.net/es-es"
+}
